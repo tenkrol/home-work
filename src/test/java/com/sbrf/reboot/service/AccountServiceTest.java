48313d849cf71a40340bab680b14adf1a25a72be
tenkr <tenkrol@gmail.com>
2021-10-24 12:17:59 +0300

    Проект собрался

--- src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
+++ src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
@@ -1,18 +1,20 @@
 package com.sbrf.reboot.service;
 
+import com.sbrf.reboot.model.Account;
+import com.sbrf.reboot.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 class AccountServiceTest {
 
-    @Mock
     AccountRepository accountRepository;
 
     AccountService accountService;
@@ -27,7 +29,7 @@ class AccountServiceTest {
     @Test
     void bookExist() {
         Account account = new Account("ACC1234NUM");
-        Set<Account> accounts = new HashSet();
+        Set<Account> accounts = new HashSet<>();
         accounts.add(account);
 
         when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
@@ -37,7 +39,7 @@ class AccountServiceTest {
 
     @Test
     void bookNotExist() {
-        Set<Account> accounts = new HashSet();
+        Set<Account> accounts = new HashSet<>();
         accounts.add(new Account("ACC1234NUM"));
 
         when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
